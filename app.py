a0f7846cecd90a9ae588c739d3619c64b7c8e7db
Caphace-Ethan <ethanyoung135@gmail.com>
2021-09-12 12:26:27 +0300

    added consumer inside app.py

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 # compose_flask/app.py
 from flask import Flask, render_template, request, redirect
 from redis import Redis
-import os, sys
+import os, sys, json
+from kafka import KafkaConsumer
 
-sys.path.append(os.path.abspath(os.path.join('scripts')))
-from scripts import consumer1
+local_boostrap_server_address = 'localhost:9092'
+
+# sys.path.append(os.path.abspath(os.path.join('scripts')))
+# from scripts import consumer1
+
+a = 0
+try:
+    consumer = KafkaConsumer(
+                    "topic0001",
+                    bootstrap_servers=local_boostrap_server_address,
+                    auto_offset_reset='latest',
+                    group_id="consumer-group-a")
+
+    a=1
+
+except:
+    a = 0
 
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
@@ -13,7 +29,7 @@ redis = Redis(host='redis', port=6379)
 def home():
     if request.method == "GET":
         e = ""
-        data_received = consumer1.get_text_corpus
+        data_received = get_text_corpus
         redis.incr('hits')
         
         return render_template("index.html", 
@@ -27,6 +43,34 @@ def home():
         
         return redirect("/")
 
+def get_text_corpus():
+    global a, consumer
+    if (a==1):
+        print("starting the consumer")
+        for msg in consumer:
+            data_received = json.loads(msg.value)
+            break
+
+
+    else:
+        try:
+            consumer = KafkaConsumer(
+                            "topic0001",
+                            bootstrap_servers=local_boostrap_server_address,
+                            auto_offset_reset='latest',
+                            group_id="consumer-group-a")
+
+            a=1
+
+            print("starting the consumer")
+            for msg in consumer:
+                data_received = json.loads(msg.value)
+                break
+
+        except:
+            data_received = "Refresh the page to get a Text"
+
+    return data_received
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
